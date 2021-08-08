716332b583ce503e1ccb24216a21d80688aebd87
BotTechMK40 <88579692+BotTechMK40@users.noreply.github.com>
2021-08-08 12:40:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,67 +3,31 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+ സുഹൃത്തേ എന്നെ നിന്റെ ഗ്രൂപ്പിലേക്ക്
+ കൊണ്ടുപോകാൻ ആണെങ്കിൽ ഉദ്ദേശം
+ അതു നടക്കില്ല കേട്ടോ എന്റെ മുതലാളിയുടെ
+ അനുവാദമില്ലാതെ എന്നെ എവിടേക്കും കൊണ്ടുവാൻ പറ്റില്ല
+ നിന്റെ ഉദ്ദേശം അതാണെങ്കിൽ വാങ്ങി വെച്ചേക്ക് <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
+<i> എന്നെ നിനക്ക് എവിടേക്കും കൊണ്ടുവാൻ പറ്റില്ല
+ പിന്നെ ഞാൻ എന്തിനാ നിന്നെ സഹായിക്കുന്നത് പിന്നെ
+ഈ സംഭവങ്ങളൊക്കെ എന്റെ മുതലാളിക്ക് മാത്രമേ കാണാൻ പറ്റുള്ളൂ
+ ഒന്നും നോക്കണ്ട വേഗം പൊക്കോ എന്റെ മുതലാളിയെ കോൺടാക്ട് ചെയ്യണമെങ്കിൽ
+/about കൊടുക്ക്   </b>!
 </i>
 
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
 
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Bot_Tech_MK
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
